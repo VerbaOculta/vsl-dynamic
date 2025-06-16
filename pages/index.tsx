71f82db17b8d0a5f0fb5abb66217272f8a9e13ce
VerbaOculta <faraya@gmail.com>
2025-06-15 23:51:25 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import VSLPlayer from "../components/VSLPlayer";
 export default function Home() {
   const playbackIds = [
     "PLLJ4CuL9LMofLBm5MZZ2Mp02NDr3y7faGatChWrqPb4",
-    "dJncnhnalgoS7khYQIHhPfzqQz9igzYHuGzUjwTBPXA"
+    "dJncnhnalgoS7khYQIHhPfzqQz9igzYHuGzUjwTBPXA",
+    "402x2M6c00d01pLzE23fvD3RMbSjxufw00RMoylhdCXF023k"
   ];
 
   return (
